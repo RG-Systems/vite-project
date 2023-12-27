29ca57066ac41534ad0a60e1dc5385b25c191f15
Robert Grigorian <dev.robert.grigoryan@gmail.com>
2023-12-28 02:25:00 +0400

    fix: [VP-1] - use only one stack

--- cdk/index.ts
+++ cdk/index.ts
@@ -8,19 +8,15 @@ import * as cdk from 'aws-cdk-lib';
 
 import { PriceClass } from 'aws-cdk-lib/aws-cloudfront';
 
-import { DistributionStack } from './distribution-stack';
-import { StorageStack } from './storage-stack';
-
-const projectName = JSON.parse(
-  fs.readFileSync(path.resolve(__dirname, '../package.json'), 'utf8')
-).name;
+import { Stack } from './stack';
 
 const app = new cdk.App();
 
 const { AWS_ACCOUNT, AWS_REGION, ORIGIN_PATH, ENV, DOMAIN, ...other } = process.env || {};
+const projectName = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../package.json'), 'utf8')).name;
 const env = (ENV || 'qa').toLowerCase().trim();
 const priceClass = env === 'prod' ? PriceClass.PRICE_CLASS_ALL : PriceClass.PRICE_CLASS_100;
-const distribution = env === 'tmp' ? `${projectName}-distribution-${env}-${ORIGIN_PATH}` : `${projectName}-distribution-${env}`;
+const stack = env === 'tmp' ? `${projectName}-${env}-${ORIGIN_PATH}` : `${projectName}-${env}`;
 const domain = env === 'tmp' ? `${ORIGIN_PATH}-${DOMAIN}` : DOMAIN;
 const variables = Object.keys(other).reduce((acc: Record<string, string>, key) => {
   if (key.startsWith('VARIABLE_')) {
@@ -31,15 +27,7 @@ const variables = Object.keys(other).reduce((acc: Record<string, string>, key) =
   return acc;
 }, {});
 
-const storageStack = new StorageStack(app, `${projectName}-storage`, {
-  env: {
-    account: AWS_ACCOUNT,
-    region: AWS_REGION,
-  }
-});
-
-new DistributionStack(app, distribution, {
-  bucket: storageStack.bucket,
+new Stack(app, stack, {
   path: ORIGIN_PATH,
   priceClass,
   variables,
@@ -48,4 +36,4 @@ new DistributionStack(app, distribution, {
     account: AWS_ACCOUNT,
     region: AWS_REGION,
   }
-}).addDependency(storageStack);
+});
